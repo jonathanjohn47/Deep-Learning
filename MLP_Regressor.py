d29be49333af89ddd05c7a69de3295b3c0774e56
Jonathan John <44868912+jonathanjohn47@users.noreply.github.com>
2018-12-07 14:34:40 +0530

    MLP Regressor
    A simple MLP Regressor code demonstrated on Boston dataset

--- MLP_Regressor.py
+++ MLP_Regressor.py
@@ -3,6 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import numpy as np
 from sklearn.datasets import load_boston
+from sklearn.metrics import mean_absolute_error, mean_squared_error
 
 boston = load_boston()
 
@@ -13,10 +14,15 @@ reg = MLPRegressor(hidden_layer_sizes=(10,),  activation='logistic', solver='ada
 
 reg = reg.fit(X, y)
 p = reg.predict(X)
-
+print(p)
 ax1 = np.arange(0, len(X[:,0])).reshape(-1,1)
 ax2 = p
 
 plt.scatter(ax1, y, color = 'blue')
 plt.plot(ax1,p, color = 'red')
 plt.show()
+
+
+print(mean_absolute_error(y, p))
+print(mean_squared_error(y, p))
+print(np.sqrt(mean_absolute_error(y, p)))
