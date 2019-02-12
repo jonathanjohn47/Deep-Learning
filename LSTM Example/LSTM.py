6750412a60f1fe0ed311ba0120c2764a5b93442c
Jonathan John <44868912+jonathanjohn47@users.noreply.github.com>
2019-02-12 11:47:57 +0530

    Update LSTM.py

--- LSTM Example/LSTM.py
+++ LSTM Example/LSTM.py
@@ -47,16 +47,6 @@ def create_dataset(dataset, look_back=1):
 		dataY.append(dataset[i + look_back, 0])
 	return numpy.array(dataX), numpy.array(dataY)
 
-# convert an array of values into a dataset matrix
-def create_dataset(dataset, look_back=1):
-	dataX, dataY = [], []
-	for i in range(len(dataset)-look_back-1):
-		a = dataset[i:(i+look_back), 0]
-		dataX.append(a)
-		dataY.append(dataset[i + look_back, 0])
-	return numpy.array(dataX), numpy.array(dataY)
-
-
 # reshape into X=t and Y=t+1
 look_back = 1
 trainX, trainY = create_dataset(train, look_back)
